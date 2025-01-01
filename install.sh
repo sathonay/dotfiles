cda46f177c7f0a4004e68d5b0f70fc4771b5210d
sathonay <>
2025-01-01 02:41:53 +0100

    fix install

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ echo "Soft linking: "
 
 
 dest="$HOME/"
+dir="$(pwd)/"
 files="$(find . -maxdepth 1 \! -type l | tail -n +2 | grep -Ewv "$(cat ignore-list)")"
-
 
 
 for file in $files; do
 	echo "linking $file"
-	ln -s $file $dest$file-soft 
+	ln -s $dir$file $dest$file 
 done
